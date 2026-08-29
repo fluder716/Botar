931120a412d524c8f10275b4543c511763b86dda
fluder716 <63340875+fluder716@users.noreply.github.com>
2026-08-29 13:11:40 +0300

    Update core_commands.php

--- Plugins/core_commands.php
+++ Plugins/core_commands.php
@@ -1,9 +1,38 @@
 <?php
 
 function command_ping(){
-    sendMessage("pong!");
+    sendMessage("البوت يعمل ✅");
 }
 
 function command_start(){
-    sendMessage("test 11 test");
+    sendMessage("أهلاً بك في BOTAR");
+}
+
+function handle_command($messageText){
+
+    if (!is_string($messageText) || trim($messageText) === "") {
+        return;
+    }
+
+    $command = trim($messageText);
+
+    // يدعم / و ! و #
+    $command = preg_replace('/^[\/!#]/', '', $command);
+
+    // يأخذ اسم الأمر فقط
+    $command = explode(' ', $command)[0];
+
+    // يدعم /start@DUBLM_BOT
+    $command = explode('@', $command)[0];
+
+    $command = strtolower($command);
+
+    if ($command === 'start') {
+
+        command_start();
+
+    } elseif ($command === 'ping') {
+
+        command_ping();
+    }
 }
